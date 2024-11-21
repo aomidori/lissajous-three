349b4a1bb6da7b1dfee0dd1c46fed750ff57342c
Hanyue <hanyuezhou16@gmail.com>
2024-11-21 23:45:53 +0100

    replace dat.gui with tweakpane

--- src/lib/data/settings.ts
+++ src/lib/data/settings.ts
@@ -1,64 +1,96 @@
-import { writable } from 'svelte/store'
-import type { GUI } from 'dat.gui';
+import { writable } from 'svelte/store';
+import { Pane } from 'tweakpane';
 import { theme } from './theme/theme';
 
-let gui: GUI | null = null;
+let pane: Pane | null = null;
 
 export type Settings = {
-  xFrequency: number;
-  yFrequency: number;
-  zFrequency: number;
-  dimension: '2D' | '3D';
-  color: number;
-}
-
-const settings: Settings = {
-  xFrequency: 1,
-  yFrequency: 0.4,
-  zFrequency: 0.5,
-  dimension: '3D',
-  color: theme.three.lineColor,
+	xFrequency: number;
+	yFrequency: number;
+	zFrequency: number;
+	dimension: '2D' | '3D';
+	color: number;
 };
 
-const settingsStore = writable<Settings>({...settings});
+const settings: Settings = {
+	xFrequency: 1,
+	yFrequency: 0.4,
+	zFrequency: 0.5,
+	dimension: '3D',
+	color: theme.three.lineColor
+};
+
+const settingsStore = writable<Settings>({ ...settings });
 
 const addGuiControls = () => {
-  if (gui) {
-    removeGuiControls();
-  }
-  import ('dat.gui').then((dat) => {
-    gui = new dat.GUI();
-    gui.addFolder('FREQUENCY');
-    gui.add(settings, 'xFrequency', 0, 1).name('X Frequency').step(0.1).onChange((value) => {
-      settingsStore.update((s) => { s.xFrequency = value; return s; });
-    });
-    gui.add(settings, 'yFrequency', 0, 1).name('Y Frequency').step(0.1).onChange((value) => {
-      settingsStore.update((s) => { s.yFrequency = value; return s; });
-    });
-    gui.add(settings, 'zFrequency', 0, 1).name('Z Frequency').step(0.1).onChange((value) => {
-      settingsStore.update((s) => { s.zFrequency = value; return s; });
-    });
-    gui.addColor(settings, 'color').name('Line Color').onChange((value) => {
-      settingsStore.update((s) => { s.color = value; return s; });
-    });
-    gui.show();
-  });
-}
+	if (pane) {
+		removeGuiControls();
+	}
+
+	pane = new Pane();
+	pane
+		.addBinding(settings, 'xFrequency', {
+			label: 'xFrequency',
+			min: 0,
+			max: 1,
+			step: 0.1
+		})
+		.on('change', (event) => {
+			settingsStore.update((s) => {
+				s.xFrequency = event.value;
+				return s;
+			});
+		});
+
+	pane
+		.addBinding(settings, 'yFrequency', {
+			label: 'yFrequency',
+			min: 0,
+			max: 1,
+			step: 0.1
+		})
+		.on('change', (event) => {
+			settingsStore.update((s) => {
+				s.yFrequency = event.value;
+				return s;
+			});
+		});
+
+	pane
+		.addBinding(settings, 'zFrequency', {
+			label: 'zFrequency',
+			min: 0,
+			max: 1,
+			step: 0.1
+		})
+		.on('change', (event) => {
+			settingsStore.update((s) => {
+				s.zFrequency = event.value;
+				return s;
+			});
+		});
+
+	const themeDir = pane.addFolder({ title: 'theme', expanded: true });
+	themeDir
+		.addBinding(settings, 'color', {
+			label: 'color',
+			view: 'color'
+		})
+		.on('change', (event) => {
+			settingsStore.update((s) => {
+				s.color = event.value;
+				return s;
+			});
+		});
+};
 
 const removeGuiControls = () => {
-  if (gui) {
-    gui.destroy();
-    gui = null;
-  }
-}
+	if (pane) {
+		pane.dispose();
+		pane = null;
+	}
+};
 
 const getSetting = (k: keyof Settings) => settings[k];
 
-export {
-  gui,
-  settings,
-  settingsStore,
-  addGuiControls,
-  removeGuiControls,
-  getSetting,
-};
+export { settings, settingsStore, addGuiControls, removeGuiControls, getSetting };
